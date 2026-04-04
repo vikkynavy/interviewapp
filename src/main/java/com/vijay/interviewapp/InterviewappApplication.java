d5b69ebba17fc5269041b591b050b5cefc9699e8
Vijay <valipireddyvijay@gmail.com>
2026-04-04 19:31:45 +0530

    Added env debug logs for datasource issue

--- src/main/java/com/vijay/interviewapp/InterviewappApplication.java
+++ src/main/java/com/vijay/interviewapp/InterviewappApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InterviewappApplication {
 
 	public static void main(String[] args) {
+
+        System.out.println("SPRING_DATASOURCE_URL=" + System.getenv("SPRING_DATASOURCE_URL"));
+        System.out.println("DATABASE_URL=" + System.getenv("DATABASE_URL"));
+
+        
 		SpringApplication.run(InterviewappApplication.class, args);
 	}
 
