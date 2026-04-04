efd5ed6b994e324ad3d9b5662b2f4ffa0373acc7
Vijay <valipireddyvijay@gmail.com>
2026-04-04 20:50:44 +0530

    fix runner visibility now

--- src/main/java/com/vijay/interviewapp/InterviewappApplication.java
+++ src/main/java/com/vijay/interviewapp/InterviewappApplication.java
@@ -7,16 +7,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class InterviewappApplication {
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     public static void main(String[] args) {
 
